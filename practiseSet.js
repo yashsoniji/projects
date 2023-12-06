5e0575cce0c0fb9b143960f98c63bbf405408255
yashsoniji <yashsoni012000@gmail.com>
2023-12-06 16:30:40 +0530

    first commit

---
+++ practiseSet.js
@@ -0,0 +1,12 @@
+const signIn = document.getElementById("signIN");
+const forgotPASS = document.getElementById("forgotPASS");
+const checkbox = document.getElementById("checkbox");
+
+
+function getNexPage() {
+    signIn.addEventListener('click')
+}
+
+
+
+
